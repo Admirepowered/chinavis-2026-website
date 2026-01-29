f5cdd9ebf8e8f3fdc5ff1f500f4eb8bed86a8868
Admirepowered <admire1971@outlook.com>
2026-01-29 16:02:44 +0800

    update base info

--- src/components/Teaser.tsx
+++ src/components/Teaser.tsx
@@ -13,7 +13,7 @@ export function Teaser() {
       <div class="absolute left-[3.5vw] top-[13vw] text-[2.8vw]">中国可视化与可视分析大会</div>
       <div class="absolute left-[3.5vw] bottom-[4vw] text-[2.2vw] font-thin">The 12th China Visualization</div>
       <div class="absolute left-[3.5vw] bottom-[2vw] text-[2.2vw] font-thin">and Visual Analytics Conference</div>
-      <div class="absolute right-[2vw] top-[22vw] text-[2.5vw] text-right font-medium">中国&centerdot;杭州</div>
+      <div class="absolute right-[2vw] top-[22vw] text-[2.5vw] text-right font-medium">中国&centerdot;贵阳</div>
       <div class="absolute right-[2vw] top-[24.6vw] text-[2vw] text-right font-thin">China&centerdot;Hangzhou</div>
       <div class="absolute right-[2vw] top-[27.6vw] text-[3.5vw] text-right leading-[2.8vw] font-bold">2026.07.19-22</div>
     </div>
