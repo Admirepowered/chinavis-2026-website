0dfc71da3a2d182f95fb41302ec86e206ef64c63
HuiResearch <m13021933043@163.com>
2026-05-25 08:48:31 +0800

    fix artprogram link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -80,7 +80,7 @@ export function Navbar() {
         <NavLink href={`/2026/${locale()}/doctoralcolloquium`} title={t("DoctoralColloquium")!} />
         <NavLink href={`/2026/${locale()}/sponsor`} title={t("Sponsorship")!} />
         <NavLink href={`/2026/${locale()}/committee`} title={t("Committee")!} />
-        <ExternalNavLink href="http://chinavisap.net/" title={t("ArtProgram")!} />
+        <ExternalNavLink href="http://chinavisap.net/2026/" title={t("ArtProgram")!} />
         <ExternalNavLink href={`https://chinavis.org/history${locale() == "en" ? "_en" : ""}.html`} title={t("History")!} />
       </div>
 
